63bc3794152636c6d75bfa4634c62991f8f4ee87
Sergio Santamaria <sergio.santamaria10@educa.madrid.org>
2025-01-22 19:31:23 +0100

    Cambios en el saludo principal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Hola, control de versiones con Git!");
+        System.out.println("¡Hola, desde la rama main!!!");
         System.out.println("Nueva funcionalidad: mensaje de bienvenida.");
     }
 }
