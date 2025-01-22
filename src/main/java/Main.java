b0d050ca5a34652cf691ef9421f1018f068e3a91
Sergio Santamaria <sergio.santamaria10@educa.madrid.org>
2025-01-22 18:57:32 +0100

    Añadido mensaje de despedida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola, control de versiones con Git!");
-        System.out.println("Nueva funcionalidad: mensaje de bienvenida.");
+        System.out.println("¡Bienvenidos al sistema de control de versiones con Git!");
+        System.out.println("Hasta luego mis panas!");
     }
 }
