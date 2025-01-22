e50ce22545a98c92cbd7dccff6e483ab3288d15f
Sergio Santamaria <sergio.santamaria10@educa.madrid.org>
2025-01-22 19:57:29 +0100

    Modificacion de la despedida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola, caracola!");
         System.out.println("Nueva funcionalidad: mensaje de bienvenida.");
-        System.out.println("Adiós!");
+        System.out.println("Hasta Luego!");
     }
 }
